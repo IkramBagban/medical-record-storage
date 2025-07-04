c4bd1bb60057d8aa45270eb3ef14ee10788f4449
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2025-07-05 01:39:15 +0530

    feat: add rate limiting middleware

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,26 @@ import express from "express";
 import { errorHandler } from "./middlewares/error.middleware";
 import v1Routes from "./routes/index";
 import dotenv from "dotenv";
+import { rateLimit } from "express-rate-limit";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: {
+    error: "Too many requests, please try again later.",
+    success: false,
+  },
+});
+
+
+app.use(limiter);
 app.use(express.json());
 app.use("/api", v1Routes);
 app.use(errorHandler);
