f02a9c722949ceee65fdad3456992235bed11584
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2025-07-12 13:03:53 +0530

    fix: update error handling for PrismaClientKnownRequestError

--- api/src/middlewares/error.middleware.ts
+++ api/src/middlewares/error.middleware.ts
@@ -1,11 +1,14 @@
-import { PrismaClientInitializationError } from "@prisma/client/runtime/library";
+import {
+  PrismaClientInitializationError,
+  PrismaClientKnownRequestError,
+} from "@prisma/client/runtime/library";
 import { Request, Response, NextFunction } from "express";
 
 export const errorHandler = (
   err: any,
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   let statusCode = null;
   let message = null;
@@ -13,6 +16,12 @@ export const errorHandler = (
   if (err instanceof PrismaClientInitializationError) {
     statusCode = 503;
     message = "Database connection error. Please try again later.";
+  } else if (err instanceof PrismaClientKnownRequestError) {
+    if (err.code === "P2002") {
+      const fields = err.meta?.target as string[] | undefined;
+      message = `Duplicate value for unique field${fields?.length === 1 ? "" : "s"}: ${fields?.join(", ")}`;
+      statusCode = 409;
+    }
   } else {
     statusCode = err.statusCode || 500;
     message = err.message || "Internal Server Error";
